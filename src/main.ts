cab116e95d380a96d00de8cd78cadb4ed1b511ae
José Jonathan <josejonathan689@gmail.com>
2021-12-05 00:29:17 -0300

    fim do projeto, deu pra testar no docker, mas utilizando as vatiaveis de ambiente não foi possivel, devido ao computador com baixa potencia que pussuo ficar parando e rodando o docker-compose foi tão cansativo que acabei dropando

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import dotenv from 'dotenv';
 import { AppModule } from './app.module';
+
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe())
+
   await app.listen(process.env.API_PORT || 3000, () => console.log('Running in the port: ', process.env.API_PORT || 3000));
 }
 bootstrap();
+
